3eed5bb10233b949089fa0a7486eefde83550d12
Hamit Güler <Hamit_g_98@hotmail.de>
2020-06-12 20:47:31 +0200

    MYSQL ansatt SQLITE
    Habe mich für MYSQL entschieden anstatt SQLite, da ich dazu mehr finde
    und ich damit besser arbeiten kann.
    
    Habe den Database Ordner gelöscht, da die Datenbanken nun auf einem
    Server sind.
    
    Habe dazu noch in der database.py es nach MYSQL angepasst und mehrere
    Standartfunktionen geschrieben.
    
    Es müssen ncoh weitere Funktionen geschrieben werden und bearbeitet
    werden.
    
    Fürs Erste ist es aber ein guter Anfang

--- Pw_Manager/source/database.py
+++ Pw_Manager/source/database.py
@@ -1,13 +1,58 @@
-import sqlite3
+import mysql.connector
+
+global mydb
+def mysql_connect():
+  mydb = mysql.connector.connect(
+    host="",
+    user="",
+    password=""
+  )
+  return mydb
 
 
-conn = sqlite3.connect('.\\..\\database\\accounts.db')
+def create_cursor(mydb):
+  mycursor = mydb.cursor()
 
-c = conn.cursor()
+  return mycursor
 
-c.execute("""CREATE TABLE accounts (
-            Username text,
-            Password text,
-            ID integer,
-            private_key text
-            )""")
+def create_database(mycursor, db_name):
+ try:
+    mycursor.execute("CREATE DATABASE " + db_name)
+ except:
+    print("Database " + db_name + " exists already")
+
+def create_table(db_name, table_name):
+    mydb = mysql.connector.connect(
+      host="",
+      user="",
+      password="",
+      database = db_name
+    )
+    mycursor = create_cursor(mydb)
+
+    try:
+      mycursor.execute("CREATE TABLE " + table_name + " (id INT AUTO_INCREMENT PRIMARY KEY, Username VARCHAR(255), Password VARCHAR(255))")
+    except:
+      print("Table " + table_name + " exists already")
+
+    return mydb
+
+def insert_table(mydb, mycursor, table_name, acc):
+    sqlformula = "INSERT INTO " + table_name + " (Username, Password) VALUES (%s, %s)"
+    mycursor.execute(sqlformula, acc)
+    mydb.commit()
+
+mydb = mysql_connect()
+print(mydb)
+
+mycursor = create_cursor(mydb)
+db_name = "storage_db"
+create_database(mycursor, db_name)
+table_name = "accounts"
+mydb = create_table(db_name, table_name)
+mycursor = create_cursor(mydb)
+acc = ("user123", "psw123")
+insert_table(mydb, mycursor, table_name, acc)
+#
+
+
